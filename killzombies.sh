c4f71dd38a11533551d76df71cce784b37ff52bf
Ellison Leão <ellisonleao@gmail.com>
2014-10-08 12:47:23 -0300

    update killzombies

--- killzombies.sh
+++ killzombies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Kill zombies processes
-kill -HUP `ps -A -ostat,ppid | grep -e '^[Zz]' | awk '{print $2}'`
+kill -9 `ps -xaw -o state -o ppid | grep Z | grep -v PID | awk '{print $2}'`
 echo "Zombies killed"
